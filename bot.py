d4d3950e51f6bb3fb1eabf9b0909384d3b26de21
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-01 21:18:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT, CHANNEL_ONE, CHANNEL_TWO
 
 
 name ="""
