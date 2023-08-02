aa10414772e5c700f8a7cb1b4a1342d7cffc8b34
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-02 18:15:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,7 +19,7 @@ from database.database import add_user, del_user, full_userbase, present_user, i
 
 
 @Bot.on_message(filters.command('start') & filters.private)
-async def start_command(client: Client, message: Message):
+async def start_command(client: Client, message):
     id = message.from_user.id
     if not await present_user(id):
         try:
