d9d84b4a2b9df89bcf8ed6b8ba740df7b7171155
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-04 20:24:09 +0530

    Update req_svd.py

--- plugins/req_svd.py
+++ plugins/req_svd.py
@@ -4,18 +4,17 @@ from bot import Bot
 from config import CHANNEL_ONE, CHANNEL_TWO
 from database.database import add_req_one, add_req_two
 
-
 @Bot.on_chat_join_request(
-    filters.chat(CHANNEL_ONE if CHANNEL_ONE else CHANNEL_TWO if CHANNEL_TWO else "self")
+    filters.chat(CHANNEL_ONE) | filters.chat(CHANNEL_TWO)
 )
 async def join_reqs(client, join_req: ChatJoinRequest):
     user_id = join_req.from_user.id
-    if CHANNEL_ONE and join_req.from_chat.id == CHANNEL_ONE:
+    if join_req.from_chat.id == CHANNEL_ONE:
         try:
             await add_req_one(user_id)
         except Exception as e:
             print(f"Error adding join request to req_one: {e}")
-    elif CHANNEL_TWO and join_req.from_chat.id == CHANNEL_TWO:
+    elif join_req.from_chat.id == CHANNEL_TWO:
         try:
             await add_req_two(user_id)
         except Exception as e:
