b4a3c4cff955c3168c27110d1a69ec7304fb8f21
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-08-04 20:26:47 +0530

    Update req_svd.py

--- plugins/req_svd.py
+++ plugins/req_svd.py
@@ -9,12 +9,12 @@ from database.database import add_req_one, add_req_two
 )
 async def join_reqs(client, join_req: ChatJoinRequest):
     user_id = join_req.from_user.id
-    if join_req.from_chat.id == CHANNEL_ONE:
+    if join_req.chat.id == CHANNEL_ONE:
         try:
             await add_req_one(user_id)
         except Exception as e:
             print(f"Error adding join request to req_one: {e}")
-    elif join_req.from_chat.id == CHANNEL_TWO:
+    elif join_req.chat.id == CHANNEL_TWO:
         try:
             await add_req_two(user_id)
         except Exception as e:
