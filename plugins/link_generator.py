e1bf19bd1b289033b0e97516195b0cf96ed1c474
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-20 16:00:40 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -7,7 +7,7 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
-async def batch(client: Client, message: Message):
+async def batch(client: Bot, message: Message):
     await message.reply("hi")
     while True:
         try:
