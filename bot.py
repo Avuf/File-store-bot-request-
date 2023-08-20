85fac7a47dc291f437dcc5a26bdad0b33fec97d9
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-21 04:44:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,11 +84,11 @@ class Bot(Client):
 ░╚════╝░░╚════╝░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚══════╝
                                           """)
         self.username = usr_bot_me.username
-        #web-response
-        #app = web.AppRunner(await web_server())
-     #   await app.setup()
-    #    bind_address = "0.0.0.0"
-     #   await web.TCPSite(app, bind_address, PORT).start()
+        web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
