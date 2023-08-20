1585dccbc7283aabf1b8b41263dcd4afd26a7c85
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-20 16:22:02 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -10,48 +10,40 @@ from helper_func import encode, get_message_id
 async def batch(client, message: Message):
     await message.reply("hi")
     try:
-        while True:
-            try:
-                first_message = await client.ask(
-                    text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
-                    chat_id=message.from_user.id,
-                    filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                    timeout=60
-                )
-            except:
-                return
-            f_msg_id = await get_message_id(client, first_message)
-            if f_msg_id:
-                break
-            else:
-                await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-                continue
+        first_message = await client.ask(
+            text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
+            chat_id=message.from_user.id,
+            filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+            timeout=60
+        )     
+    except:
+        return
+    f_msg_id = await get_message_id(client, first_message)
+        if not f_msg_id:
+            return await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+    while True:
+        try:
+            second_message = await client.ask(
+                text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
+                chat_id=message.from_user.id,
+                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
+                timeout=60
+            )
+        except:
+            return
+        s_msg_id = await get_message_id(client, second_message)
+        if s_msg_id:
+            break
+        else:
+            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
 
-        while True:
-            try:
-                second_message = await client.ask(
-                    text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
-                    chat_id=message.from_user.id,
-                    filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                    timeout=60
-                )
-            except:
-                return
-            s_msg_id = await get_message_id(client, second_message)
-            if s_msg_id:
-                break
-            else:
-                await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-                continue
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
-        string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-        base64_string = await encode(string)
-        link = f"https://t.me/{client.username}?start={base64_string}"
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-        await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
-    except Exception as e:
-        print(e)
-        
 @Client.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client, message: Message):
     while True:
