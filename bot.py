45c9b99513c40a1eb5156f556296cef882a1f028
Avuf <127880889+Avuf@users.noreply.github.com>
2025-02-15 23:37:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
 #(©)Codexbotz
+# fix for current pyrogram 
+from pyrogram import utils
 
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+utils.get_peer_type = get_peer_type_new
 from aiohttp import web
 from plugins import web_server
 
