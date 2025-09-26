e2bdfd158ebdd077ca2b15ebf4bca1f59c642707
Avuf <127880889+Avuf@users.noreply.github.com>
2025-09-27 00:42:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_HASH = os.environ.get("API_HASH", "6ff44fffc149a6dc599a5d2eaeb8873c")
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001848530957"))
 
-CHANNEL_ONE = int(os.environ.get("CHANNEL_ONE", "-1003030311155"))
+CHANNEL_ONE = int(os.environ.get("CHANNEL_ONE", "-1002990720982"))
 CHANNEL_TWO = int(os.environ.get("CHANNEL_TWO", "-1002911467634"))
 
 #OWNER ID
